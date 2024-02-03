d6dcc3c7ad1130d1cb34ff1941b82d8af3ed070d
Zhursinbek Nurbolat <jursinbek03gemail.ru@gmail.com>
2024-02-03 17:31:45 +0600

    first commit

---
+++ Hotel.ATR.FakePortal/Controllers/RoomController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hotel.ATR.FakePortal.Controllers
+{
+    public class RoomController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+       public IActionResult RoomDetails()
+        {
+            return View();
+        }
+        public IActionResult RoomList()
+        {
+            return View();
+        }
+    }
+}
+
