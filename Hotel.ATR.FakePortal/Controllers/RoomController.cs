ec99a40a5b4a16e5b6da899d7bca860de8345c2a
Zhursinbek Nurbolat <jursinbek03gemail.ru@gmail.com>
2024-02-10 16:39:06 +0600

    commit

--- Hotel.ATR.FakePortal/Controllers/RoomController.cs
+++ Hotel.ATR.FakePortal/Controllers/RoomController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hotel.ATR.FakePortal.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,8 +11,14 @@ namespace Hotel.ATR.FakePortal.Controllers
 {
     public class RoomController : Controller
     {
+        private IWebHostEnvironment webHost;
+
+        public RoomController(IWebHostEnvironment webHost)
+        {
+            this.webHost = webHost;
+            }
         // GET: /<controller>/
-        public IActionResult Index()
+        public IActionResult Index(int page, int counter)
         {
             return View();
         }
@@ -24,6 +31,19 @@ namespace Hotel.ATR.FakePortal.Controllers
         {
             return View();
         }
-    }
+        // string email User user
+        [HttpPost]
+        public IActionResult SubscribeNewsLetter(IFormFile userfile)
+        {
+            var data = Request.Form["email"];
+            string path = Path.Combine(webHost.WebRootPath,userfile.FileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                userfile.CopyTo(stream);
+            }
+            return View();
+            }
+        }
+
 }
 
---
+++ Hotel.ATR.FakePortal/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Hotel.ATR.FakePortal.Models
+{
+	public class User
+	{
+        public string email { get; set; }
+        //public string name { get; set; }
+    }
+}
+
